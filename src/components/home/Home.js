767bd82c3a2820db83b1447b4499a510f75ddca4
unknown <aadarsh.wdt@gmail.com>
2021-12-14 19:18:54 +0530

    add header

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,14 +3,14 @@ import Banner from './Banner';
 import Play from './Play';
 import Sports from './Sports';
 import Challenges from './Challenges';
-// import Header from "../header/Header";
+import Header from "../header/Header";
 
 function Home() {
     return (
         <>
             <div className="home">
                 <div id="wrap">
-                    
+                <Header/>
                     <Banner />
                     <Play />
                     <Sports />
